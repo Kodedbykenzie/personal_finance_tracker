a040d0d2c8ba85a7a949f401c9a624eb15497514
Raj Gada <rgada28@gmail.com>
2023-10-01 13:01:50 +0530

    Added  Form based login to Signup page

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -3,8 +3,41 @@ import 'package:lottie/lottie.dart';
 import 'package:personal_finance_tracker/home_screen.dart';
 import 'package:personal_finance_tracker/login_page.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final GlobalKey<FormState> _formkey = GlobalKey();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _usernameController.dispose();
+    _phoneNumberController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  signup() {
+    if (_formkey.currentState!.validate()) {
+      _formkey.currentState!.save();
+    }
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) {
+      return const HomePage();
+    }), (route) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,98 +45,170 @@ class SignUpPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Sign Up'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Inside SignUpPage build method
-              Lottie.asset(
-                'assets/animation_lml0j212.json', // Replace with your animation file path
-                width: 200,
-                height: 200,
-                repeat: true, // Set to true if you want the animation to loop
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'First Name',
-                  icon: Icon(Icons.person),
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Inside SignUpPage build method
+                Lottie.asset(
+                  'assets/animation_lml0j212.json', // Replace with your animation file path
+                  width: 200,
+                  height: 200,
+                  repeat: true, // Set to true if you want the animation to loop
                 ),
-                keyboardType: TextInputType.text,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Last Name',
-                  icon: Icon(Icons.person),
+                TextFormField(
+                  controller: _firstNameController,
+                  decoration: const InputDecoration(
+                    labelText: 'First Name',
+                    icon: Icon(Icons.person),
+                  ),
+                  keyboardType: TextInputType.text,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter your first name";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    // You can add logic here to save the value
+                    _firstNameController.clear();
+                  },
                 ),
-                keyboardType: TextInputType.text,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  icon: Icon(Icons.account_circle),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _lastNameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                    icon: Icon(Icons.person),
+                  ),
+                  keyboardType: TextInputType.text,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter your last name";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    // You can add logic here to save the value
+                    _lastNameController.clear();
+                  },
                 ),
-                keyboardType: TextInputType.text,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Phone Number',
-                  icon: Icon(Icons.phone),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    icon: Icon(Icons.account_circle),
+                  ),
+                  keyboardType: TextInputType.text,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter your Username";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    // You can add logic here to save the value
+                    _usernameController.clear();
+                  },
                 ),
-                keyboardType: TextInputType.phone,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  icon: Icon(Icons.email),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _phoneNumberController,
+                  decoration: const InputDecoration(
+                    labelText: 'Phone Number',
+                    icon: Icon(Icons.phone),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value == null ||
+                        value.trim().isEmpty ||
+                        value.trim().length < 10) {
+                      return "Please enter a valid Phone number";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    // You can add login here to save the value
+                    _phoneNumberController.clear();
+                  },
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  icon: Icon(Icons.lock),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    icon: Icon(Icons.email),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null ||
+                        value.trim().isEmpty ||
+                        !value.contains('@')) {
+                      return "Please enter a valid email address";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    _emailController.clear();
+                  },
                 ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Implement your signup logic here
-                  // Typically, you'd send the user's data to a server or database
-                  // and handle the response accordingly.
-                  // For this example, we'll show a success message.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Sign Up Successful!'),
-                    ),
-                  );
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    icon: Icon(Icons.lock),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.trim().length < 6) {
+                      return "Password should contain atleast characters";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    // You can add login here to save the value
+                    _passwordController.clear();
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement your signup logic here
+                    // Typically, you'd send the user's data to a server or database
+                    // and handle the response accordingly.
+                    // For this example, we'll show a success message.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Sign Up Successful!'),
+                      ),
+                    );
 
-                  // After successful signup, navigate to the login page
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => const HomePage()),
-                  );
-                },
-                child: const Text('Sign Up'),
-              ),
-              const SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Navigate to the login page directly
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => const LoginPage()),
-                  );
-                },
-                child: const Text('Already have an account? Log In'),
-              ),
-            ],
+                    // After successful signup, navigate to the Home page
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => const HomePage()),
+                    );
+                  },
+                  child: const Text('Sign Up'),
+                ),
+                const SizedBox(height: 16.0),
+                TextButton(
+                  onPressed: () {
+                    // Navigate to the login page directly
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => const LoginPage()),
+                    );
+                  },
+                  child: const Text('Already have an account? Log In'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
